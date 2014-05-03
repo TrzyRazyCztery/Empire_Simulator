32f5144fc388a895c2d53d496c5b62e516f5d40c
TrzyRazyCztery <3x4to9@gmail.com>
2014-05-03 23:38:51 +0200

    klasa swiata

---
+++ Empire_Simulator/Empire_Simulator/Klasy/Swiat/Swiat.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Empire_Simulator
+{
+    class Swiat
+    {
+        private List<Osada> listOsad;
+        private List<Handlarz> listaHandlarzy;
+
+        public Swiat()
+        {
+            listaHandlarzy = new List<Handlarz>();
+            listOsad = new List<Osada>();
+        }
+
+        public void dodajOsade(Osada osada)
+        {
+            this.listOsad.Add(osada);
+        }
+
+        public void dodajHandlarza(Handlarz handlarz)
+        {
+            this.listaHandlarzy.Add(handlarz);
+        }
+
+        public List<Handlarz> pobierzListeHandlarzy()
+        {
+            return listaHandlarzy;
+        }
+
+        public List<Osada> pobierzListeOsad()
+        {
+            return listOsad;
+        }
+    }
+}
